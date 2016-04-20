11759aa266caccab22cc48a68791e6abb33b2efe
David Lokshin <dlokshin@gmail.com>
2016-04-20 10:25:20 -0700

    Update dlokshin.js

--- dlokshin.js
+++ dlokshin.js
@@ -203,6 +203,8 @@ var beaches =
 ];
 
 $(document).ready(function () {
+  var divAppend = '<div class="box"></div><div class="box"><p>Teaser</p><iframe src="http://surf.traceup.com/surfline/scouts?trailer=true" height="212" width="630">You need a Frames Capable browser to view this content.</iframe></div>'
+  $('body').append(divAppend);
   for (i = 0; i < beaches.length; i++){
     var divAppend = '<div class="box"><p>' + beaches[i]['TITLE'] + '</p><iframe src="http://surf.traceup.com/surfline/scouts?breakId=' + beaches[i]['ID'] + '" height="212" width="630">You need a Frames Capable browser to view this content.</iframe></div>'
     $('body').append(divAppend);
