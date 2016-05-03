a98829ec9dc287638551a5710702f3ef82339004
David Lokshin <dlokshin@gmail.com>
2016-05-03 15:56:40 -0700

    testing this ish

---
+++ test.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+  var divAppend = '<div class="box"></div><div class="box"><p>Teaser</p><iframe src="http://surf.traceup.com/surfline/scouts?trailer=true" height="212" width="630">You need a Frames Capable browser to view this content.</iframe></div>'
+  $('body').append(divAppend);
+});
